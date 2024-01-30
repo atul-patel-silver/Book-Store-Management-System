8ce32ff31616e6c896f49d8475f04604ab7d031a
atul-patel-silver <atulpatel2001a@gmail.com>
2024-01-30 13:35:17 +0530

    init

---
+++ src/main/java/com/book/store/restcontroller/HomeController.java
@@ -0,0 +1,14 @@
+package com.book.store.restcontroller;
+
+import com.book.store.payload.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+
+    public ResponseEntity<ApiResponse> registerCustomer()
+}
